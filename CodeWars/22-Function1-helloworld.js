332744f90c2627a7867e6b4b348507aad8541636
Azka <azka@students.amikom.ac.id>
2024-02-18 03:13:45 +0700

    CodeWars(day22): Function 1 - hello world

---
+++ CodeWars/22-Function1-helloworld.js
@@ -0,0 +1,17 @@
+/**
+ * TODO:
+ * CodeWars Challenge
+ * Link: https://www.codewars.com/kata/523b4ff7adca849afe000035/train/javascript
+ * Title: Function 1 - hello world
+ * Language: JavaScript
+ * Submitted: 18/02/2024
+ * Challenge Day: 22
+ * Author: Azka
+ * Message for the Day: "Hello world!"
+ */
+
+function greet() {
+    return "hello world!";
+}
+
+console.log(greet()); // should return "hello world!"
